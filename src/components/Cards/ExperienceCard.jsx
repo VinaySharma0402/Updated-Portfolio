ea98170b3b6199026db43b25b227daee2ecdabd4
Vinay Kumar <vinaysharma8548@gmail.com>
2026-02-04 03:34:54 +0530

    Fixed all remaining ESLint errors for Vercel build

--- src/components/Cards/ExperienceCard.jsx
+++ src/components/Cards/ExperienceCard.jsx
@@ -12,19 +12,7 @@ const Document = styled.img`
         opacity: 0.8;
     }
 `
-const DocumentButton = styled.button`
-    height: 70px;
-    width: fit-content;
-    background-color: #000;
-    color: #fff;
-    border-radius: 10px;
-    border: none;
-    padding: 10px;
-    &:hover{
-        cursor: pointer;
-        opacity: 0.8;
-    }
-`
+// DocumentButton removed - not used
 
 const Description = styled.div`
     width: 100%;
--- src/components/HeroSection/HeroStyle.js
+++ src/components/HeroSection/HeroStyle.js
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import _default from "../../themes/default";
+// Theme import removed - not used
 
 export const HeroContainer = styled.div`
   background: ${({ theme }) => theme.card_light};
